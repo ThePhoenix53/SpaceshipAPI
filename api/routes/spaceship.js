8f01313d54c9c0fc74eaa1a7bddd2c925167759a
Marc Kohlmann <marc.leonard.kohlmann@gmail.com>
2020-11-30 17:22:08 +1100

    Adding basic routes to spaceship endpoint

--- api/routes/spaceship.js
+++ api/routes/spaceship.js
@@ -5,9 +5,34 @@ const router = express.Router();
 router.get('/', (req, res, next) =>{
     res.status(200).json({
         message: "Handeling GET for spaceship"
-    })
-})
+    });
+});
 
+router.post('/', (req, res, next) =>{
+    res.status(200).json({
+        message: "Handeling POST for spaceship"
+    });
+});
 
+router.get('/:spaceshipId', (req, res, next) =>{
+    const id = req.params.productId;
+    res.status(200).json({
+        message: "Handeling GET for spaceship: " + id
+    });
+});
+
+router.patch('/:spaceshipId', (req, res, next) =>{
+    const id = req.params.productId;
+    res.status(200).json({
+        message: "Handeling PATCH for spaceship: " + id
+    });
+});
+
+router.delete('/:spaceshipId', (req, res, next) =>{
+    const id = req.params.productId;
+    res.status(200).json({
+        message: "Handeling DELETE for spaceship: " + id
+    });
+});
 
 module.exports = router;
