efa6fafd37fd862aa48975b5fec97f0a939f81c8
Marc Kohlmann <marc.leonard.kohlmann@gmail.com>
2020-11-30 17:45:40 +1100

    Adding comments for clarity

--- api/routes/location.js
+++ api/routes/location.js
@@ -2,18 +2,24 @@
 const express = require('express')
 const router = express.Router();
 
+//GET request handeler
+//Get all the locations on the system
 router.get('/', (req, res, next) =>{
     res.status(200).json({
         message: "Handeling GET for location"
     });
 });
 
+//POST request handler
+//Make a new location
 router.post('/', (req, res, next) =>{
     res.status(201).json({
         message: "Handeling POST for location"
     });
 });
 
+//GET request handler
+//get details of one planet
 router.get('/:planetId', (req, res, next) =>{
     const id = req.params.planetId;
     res.status(201).json({
@@ -21,6 +27,8 @@ router.get('/:planetId', (req, res, next) =>{
     });
 });
 
+//DELETE request handler
+//delete given location
 router.delete('/:planetId', (req, res, next) =>{
     const id = req.params.planetId;
     res.status(201).json({
--- api/routes/spaceship.js
+++ api/routes/spaceship.js
@@ -2,18 +2,24 @@
 const express = require('express')
 const router = express.Router();
 
+//GET Request Handeler
+//Returns all space ships
 router.get('/', (req, res, next) =>{
     res.status(200).json({
         message: "Handeling GET for spaceship"
     });
 });
 
+//POST Request Handeler
+//Builds new space ship
 router.post('/', (req, res, next) =>{
     res.status(201).json({
         message: "Handeling POST for spaceship"
     });
 });
 
+//GET request handler for spaceships
+//retuns single spaceship infomation
 router.get('/:spaceshipId', (req, res, next) =>{
     const id = req.params.productId;
     res.status(200).json({
@@ -21,6 +27,8 @@ router.get('/:spaceshipId', (req, res, next) =>{
     });
 });
 
+//PATCH request handler
+//update the status of a single ship
 router.patch('/:spaceshipId', (req, res, next) =>{
     const id = req.params.productId;
     res.status(200).json({
@@ -28,6 +36,8 @@ router.patch('/:spaceshipId', (req, res, next) =>{
     });
 });
 
+//DELETE request handeler
+//delete spaceship from database
 router.delete('/:spaceshipId', (req, res, next) =>{
     const id = req.params.productId;
     res.status(200).json({
