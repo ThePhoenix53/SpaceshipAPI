4da493b8bbe27fe14070c1ea430950139f4cff18
Marc Kohlmann <marc.leonard.kohlmann@gmail.com>
2020-12-01 17:40:55 +1100

    Connected Spaceship with database

---
+++ api/models/spaceship.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const spaceshipSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {type: String, required: true},
+    model: {type: String, required: true},
+    location: {type: mongoose.Schema.Types.ObjectId, ref: "Location", required: true},//mongoose.Schema.Types.ObjectId,
+    status: {type: String, required: true},
+})
+
+module.exports = mongoose.model('Spaceship', spaceshipSchema)
--- api/routes/spaceship.js
+++ api/routes/spaceship.js
@@ -8,29 +8,68 @@ const Spaceship = require('../models/spaceship')
 //GET Request Handeler
 //Returns all space ships
 router.get('/', (req, res, next) =>{
-    Spaceship.find({})
+    //Find all spaceships in database
+    Spaceship.find({}, 'name model _id location status')
     .exec()
     .then(docs => {
-        res.status(200).json(docs);
+        const response = {
+            //Format response
+            count: docs.length,
+            spaceship: docs.map(doc => {
+                return {
+                    _id: doc._id,
+                    name: doc.name,
+                    model: doc.model,
+                    location: doc.location,
+                    status: doc.status,
+                    request: {
+                        type: 'GET',
+                        url: 'http://120.152.21.208/spaceship/' + doc._id
+                    }
+                }
+            })
+        }
+        res.status(200).json(response);
+    
     })
     .catch(err => {
-        res.status(500)
+        console.log(err);
+        res.status(500).json({error: err})
     });
-    
 });
 
 //POST Request Handeler
 //Builds new space ship
 router.post('/', (req, res, next) =>{
-    const spaceship = {
+    const spaceship = new Spaceship({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         model: req.body.model,
         location: req.body.location,
         status: req.body.status
-    }
-    res.status(201).json({
-        message: "Handeling POST for spaceship",
-        createdSpaceship: spaceship
+    });
+    
+    spaceship.save()
+    .then(result => {
+        //Respons to request
+        res.status(201).json({
+            message: 'Created spaceship successfully',
+            createdSpaceship: {
+                name: result.name,
+                model: result.model,
+                location: result.location,
+                status: result.status,
+            },
+            request: {
+                type: 'GET',
+                url: 'http://120.152.21.208/spaceship/' + result._id
+            }
+        });
+    })
+    .catch(err => {
+        if (!res.writableEnded){
+            res.status(500).json({error: err});
+        }
     });
 });
 
